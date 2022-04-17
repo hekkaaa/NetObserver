f00e6b9e5b9757f1c9986209d41dd14d03eaedf1
Alexander Pototsky <silencemyalise@gmail.com>
2022-04-17 09:27:56 +0600

    Add frist Class Ping and tests

---
+++ NetObserver/PingUtility/PingClassic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+
+namespace NetObserver.PingUtility
+{
+    public class PingClassic
+    {
+
+        public List<PingReply> PingRequest(string hostname)
+        {
+            PingIcmp ping = new PingIcmp();
+            List<PingReply> pingReplyListReturn = new List<PingReply>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                pingReplyListReturn.Add(ping.PingRequest(hostname));
+            }
+
+            return pingReplyListReturn;
+        }
+
+        public List<PingReply> PingRequest(string hostname, int repeat)
+        {
+            PingIcmp ping = new PingIcmp();
+            List<PingReply> pingReplyListReturn = new List<PingReply>();
+
+            for (int i = 0; i < repeat; i++)
+            {
+                pingReplyListReturn.Add(ping.PingRequest(hostname));
+            }
+
+            return pingReplyListReturn;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# EN-US
+Auxiliary library for simplified work with tracing and ping.
+The library is written according to the technology: .NET standard 2.0
+
+# RU
+Вспомогательная библиотека для упрощеной работы с трассировкой и пингом.
+Библиотека написана по технологии: .NET standard 2.0
+
+# 中国
+用于简化跟踪和 ping 工作的辅助库.
+该库是根据技术编写的： .NET standard 2.0
