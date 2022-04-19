1c802e322d43317a3606cdea0cbd7a522890f45c
Alexander Pototsky <silencemyalise@gmail.com>
2022-04-19 12:32:38 +0600

    add new Class Traceroute and a number of methods.

--- NetObserver/PingUtility/PingIcmp.cs
+++ NetObserver/PingUtility/PingIcmp.cs
@@ -35,7 +35,7 @@ namespace NetObserver.PingUtility
                 }
                 catch (ArgumentOutOfRangeException ex)
                 {
-                    throw new ArgumentOutOfRangeException("Timeout is less than zero.", ex.InnerException);
+                    throw new ArgumentOutOfRangeException("Timeout is less than zero.", ex.Message);
                 }
                 catch (PingException ex)
                 {
@@ -77,7 +77,52 @@ namespace NetObserver.PingUtility
                 }
                 catch(ArgumentOutOfRangeException ex)
                 {
-                    throw new ArgumentOutOfRangeException(hostname, ex.InnerException);
+                    throw new ArgumentOutOfRangeException(hostname, ex.Message);
+                }
+                catch (PingException ex)
+                {
+                    throw new PingException("An exception was thrown while sending or receiving the ICMP messages. See the inner exception for the exact exception that was thrown.", ex.InnerException);
+                }
+                catch (ObjectDisposedException ex)
+                {
+                    throw new ObjectDisposedException("This object has been disposed.", ex.InnerException);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Unexpected error" + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A preliminary request to send ICMP messages to the specified computer and receive a response from it. This method allows you to specify a timeout for the operation.
+        /// </summary>
+        /// <param name="hostname">The address of the remote host from which you want to receive a response.</param>
+        /// <param name="timeout">An Int32 value that specifies the maximum time (after sending ping messages) to wait for an ICMP ping message, in milliseconds.</param>
+        /// <param name="buffer">A Byte[] array that contains data to be sent with the ICMP echo message and returned in the ICMP echo reply message. The array cannot contain more than 65,500 bytes.</param>
+        /// <param name="options">A PingOptions object used to control fragmentation and Time-to-Live values for the ICMP echo message packet.</param>
+        /// <exception cref="ArgumentNullException">Hostname is null or is an empty string ("").</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Timeout is less than zero.</exception>
+        /// <exception cref="PingException">An exception was thrown while sending or receiving the ICMP messages. See the inner exception for the exact exception that was thrown.</exception>
+        /// <exception cref="ObjectDisposedException">This object has been disposed.</exception>
+        /// <exception cref="Exception">Unexpected error.</exception>
+        /// <returns>A <see cref="PingReply"/> objects that provides information  about the ICMP ping response message, if one was received, or the reason for the failure if the message was not received.</returns>
+        public PingReply PingRequest(string hostname, int timeout, byte[] buffer, PingOptions options)
+        {
+            using (Ping ping = new Ping())
+            {
+                try
+                {
+                    PingReply result = ping.Send(hostname, timeout, buffer, options);
+                    return result;
+                }
+                catch (ArgumentNullException ex)
+                {
+                    throw new ArgumentNullException(@"Hostname is null or is an empty string ("").", ex.InnerException);
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    throw new ArgumentOutOfRangeException(hostname, ex.Message);
                 }
                 catch (PingException ex)
                 {
---
+++ NetObserver/TracerouteUtility/Traceroute.cs
@@ -0,0 +1,106 @@
+﻿using NetObserver.PingUtility;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace NetObserver.TracerouteUtility
+{
+    /// <summary>
+    /// Allows an application to determine a route to a destination by sending ICMP (Internet Control Protocol) echo packets to the destination.
+    /// </summary>
+    public class Traceroute
+    {
+        private int _timeout = 4000; // default timeout https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ping
+        private int _maxttl = 30;
+        private byte[] _buffer = new byte[32]; // default value byte https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ping 
+        private bool _fragment = false;
+
+
+        public IEnumerable<string> GetIpTraceRoute(string hostname)
+        {
+            PingIcmp pingSender = new PingIcmp();
+            List<string> resultList = new List<string>();
+
+            for (var ttl = 1; ttl <= _maxttl; ttl++)
+            {
+                PingReply reply = pingSender.PingRequest(hostname, _timeout, _buffer, new PingOptions { Ttl = ttl, DontFragment = _fragment });
+                if (reply.Status == IPStatus.Success)
+                {
+                    resultList.Add(reply.Address.ToString());
+                    break;
+                }
+                else if (reply.Status == IPStatus.TtlExpired)
+                {
+                    resultList.Add(reply.Address.ToString());
+                }
+            }
+            return resultList;
+        }
+
+        public IEnumerable<string> GetIpTraceRoute(string hostname, int timeout, byte[] buffer, PingOptions option, int maxtll = 30)
+        {
+            PingIcmp pingSender = new PingIcmp();
+            List<string> resultList = new List<string>();
+
+            for (var ttl = 1; ttl <= maxtll; ttl++)
+            {
+                PingReply reply = pingSender.PingRequest(hostname, timeout, buffer, option);
+                if (reply.Status == IPStatus.Success)
+                {
+                    resultList.Add(reply.Address.ToString());
+                    break;
+                }
+                else if (reply.Status == IPStatus.TtlExpired)
+                {
+                    resultList.Add(reply.Address.ToString());
+                }
+            }
+            return resultList;
+        }
+
+        public IEnumerable<PingReply> GetDetailTraceRoute(string hostname)
+        {
+            PingIcmp pingSender = new PingIcmp();
+            List<PingReply> resultList = new List<PingReply>();
+
+            for (var ttl = 1; ttl <= _maxttl; ttl++)
+            {
+                PingReply reply = pingSender.PingRequest(hostname, _timeout, _buffer, new PingOptions { Ttl = ttl, DontFragment = _fragment });
+                if (reply.Status == IPStatus.Success)
+                {
+                    PingReply result = pingSender.PingRequest(reply.Address.ToString(), _timeout);
+                    resultList.Add(result);
+                    break;
+                }
+                else if (reply.Status == IPStatus.TtlExpired)
+                {
+                    PingReply result = pingSender.PingRequest(reply.Address.ToString(), _timeout);
+                    resultList.Add(result);
+                }
+            }
+            return resultList;
+        }
+
+        public IEnumerable<PingReply> GetDetailTraceRoute(string hostname, int timeout, byte[] buffer, PingOptions option, int maxtll = 30)
+        {
+            PingIcmp pingSender = new PingIcmp();
+            List<PingReply> resultList = new List<PingReply>();
+
+            for (var ttl = 1; ttl <= maxtll; ttl++)
+            {
+                PingReply reply = pingSender.PingRequest(hostname, timeout, buffer, option);
+                if (reply.Status == IPStatus.Success)
+                {
+                    PingReply result = pingSender.PingRequest(reply.Address.ToString(), _timeout);
+                    resultList.Add(result);
+                    break;
+                }
+                else if (reply.Status == IPStatus.TtlExpired)
+                {
+                    PingReply result = pingSender.PingRequest(reply.Address.ToString(), _timeout);
+                    resultList.Add(result);
+                }
+            }
+            return resultList;
+        }
+    }
+}
