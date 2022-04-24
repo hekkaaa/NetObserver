b66960af14a70dfcaaa4c9d99307ab0101a3662f
Alexander Pototsky <silencemyalise@gmail.com>
2022-04-24 13:25:13 +0600

    Add class LocalIp.cs

---
+++ NetObserver/IpAdressUtility/LocalIp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace NetObserver.IpAdressUtility
+{
+    public class LocalIp
+    {
+        private static List<string> _listIp = new List<string>();
+
+        public static string GetIpLocalhost()
+        {
+            AddIpWithDhcpPrefix();
+            AddIpWithManualPrefix();
+
+            return _listIp.FirstOrDefault()?.ToString();
+        }
+
+        private static void AddIpWithDhcpPrefix()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface nic in nics)
+            {
+                if (nic.GetIPProperties().UnicastAddresses.LastOrDefault()?.PrefixOrigin == System.Net.NetworkInformation.PrefixOrigin.Dhcp)
+                {
+                    IPAddress ip = nic.GetIPProperties().UnicastAddresses.LastOrDefault()?.Address;
+                    _listIp.Add(ip.ToString());
+                }
+            }
+        }
+
+        private static void AddIpWithManualPrefix()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface nic in nics)
+            {
+                if (nic.GetIPProperties().UnicastAddresses.LastOrDefault()?.PrefixOrigin == System.Net.NetworkInformation.PrefixOrigin.Manual)
+                {
+                    IPAddress ip = nic.GetIPProperties().UnicastAddresses.LastOrDefault()?.Address;
+                    _listIp.Add(ip.ToString());
+                }
+            }
+        }
+    }
+}
